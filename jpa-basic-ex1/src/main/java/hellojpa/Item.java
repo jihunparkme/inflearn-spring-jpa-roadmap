0fc82b6822b5cf20213678626e952e70d9e61397
Jihun Park <jihunpark.tech@gmail.com>
2022-04-03 10:26:41 +0900

    [고급 매핑] 상속관계 매핑

---
+++ jpa-basic-ex1/src/main/java/hellojpa/Item.java
@@ -0,0 +1,15 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn
+public class Item {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+    private Integer price;
+}
