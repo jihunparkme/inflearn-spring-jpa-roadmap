ec66e292ac6a4e2e26d9a3973a864fd759efad35
Jihun Park <jihunpark.tech@gmail.com>
2022-04-11 22:56:50 +0900

    [값 타입] 값 타입 매핑

--- jpa-basic-jpashop/src/main/java/jpbook/jpashop/domain/Member.java
+++ jpa-basic-jpashop/src/main/java/jpbook/jpashop/domain/Member.java
@@ -11,50 +11,42 @@ public class Member extends BaseEntity {
     @Column(name = "member_id ")
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getCity() {
-        return city;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getStreet() {
-        return street;
+    public Address getAddress() {
+        return address;
     }
 
-    public String getZipcode() {
-        return zipcode;
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
     }
 }
