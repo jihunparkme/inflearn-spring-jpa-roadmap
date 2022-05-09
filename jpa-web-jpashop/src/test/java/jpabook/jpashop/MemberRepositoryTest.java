fb6b8e2e4be979a90c8e9344c5962383cbbf960e
Jihun Park <jihunpark.tech@gmail.com>
2022-05-09 19:47:02 +0900

    add: [도메인 분석 설계] 엔티티 클래스 개발 1

--- jpa-web-jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpa-web-jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @PropertySource("classpath:application.yml")
