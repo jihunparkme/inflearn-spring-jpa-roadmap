36ae29aca1e9830fcf665da85cba79277a0e5e54
Jihun Park <jihunpark.tech@gmail.com>
2022-05-16 22:55:32 +0900

    add: [회원 도메인 개발] 회원 서비스 개발

---
+++ jpa-web-jpashop/src/main/java/jpabook/jpashop/Service/MemberService.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        // 멀티 쓰레드 상황을 고려하여 최후의 방어로 Member name 을 unique 제약 조건으로 설정하자.
+        if (findMembers.size() > 0) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
