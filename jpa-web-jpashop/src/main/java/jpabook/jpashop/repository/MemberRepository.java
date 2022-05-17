797afa40d33969fa1f58c2998bb500aaace2c412
Jihun Park <jihunpark.tech@gmail.com>
2022-05-17 23:04:24 +0900

    fix: @PersistenceContext 대신 final, RequiredArgsConstructor (@Autowired)로 대체

--- jpa-web-jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpa-web-jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,17 +1,21 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    /**
+     * SpringBoot(SpringDataJPA) 가
+     * @PersistenceContext 대신 final, RequiredArgsConstructor (@Autowired)로 대체 가능하도록 지원
+     */
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
