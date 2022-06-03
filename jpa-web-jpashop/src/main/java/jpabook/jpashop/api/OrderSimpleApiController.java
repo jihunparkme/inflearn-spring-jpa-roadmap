92fc723c3593017fbe7904423a5f9c30457aa108
Jihun Park <jihunpark.tech@gmail.com>
2022-06-03 23:00:11 +0900

    add: (지연 로딩과 조회 성능 최적화) 간단한 주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화

--- jpa-web-jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpa-web-jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 /*
  * xToOne(ManyToOne, OneToOne) 관계 최적화
@@ -49,7 +50,21 @@ public class OrderSimpleApiController {
     public List<SimpleOrderDto> ordersV2() {
         List<SimpleOrderDto> result = orderRepository.findAllByString(new OrderSearch()).stream()
                 .map(SimpleOrderDto::new)
-                .collect(Collectors.toList());
+                .collect(toList());
+
+        return result;
+    }
+
+    /**
+     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+     * - fetch join으로 쿼리 1번 호출
+     * 참고: fetch join에 대한 자세한 내용은 JPA 기본편 참고(정말 중요함)
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<SimpleOrderDto> result = orderRepository.findAllWithMemberDelivery().stream()
+                .map(SimpleOrderDto::new)
+                .collect(toList());
 
         return result;
     }
