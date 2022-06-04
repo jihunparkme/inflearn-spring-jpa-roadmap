cc35104529ac8183cab2b964b32f86eff4fa6ddb
Jihun Park <jihunpark.tech@gmail.com>
2022-06-04 13:40:31 +0900

    add: (지연 로딩과 조회 성능 최적화) 간단한 주문 조회 V4: JPA에서 DTO로 바로 조회

--- jpa-web-jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpa-web-jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.OrderSimpleQueryDto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -67,6 +68,16 @@ public class OrderSimpleApiController {
                 .collect(toList());
 
         return result;
+    }
+
+    /**
+     * V4. JPA에서 DTO로 바로 조회
+     * - 쿼리 1번 호출
+     * - select 절에서 원하는 데이터만 선택해서 조회
+     */
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderRepository.findOrderDtos();
     }
 
     @Data
